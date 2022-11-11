ebd49c62c13e376e6d044dcec86abf2307f74e08
saraabed <saraalabad4@gmail.com>
2022-11-11 19:22:39 +0200

    meaningfull comment

---
+++ sara1/app/Http/Controllers/TaskController.php
@@ -0,0 +1,135 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $tasks = Task::orderBy('id', 'desc')->get();
+        return view('index', compact('tasks'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $statuses = [
+            [
+                'label' => 'Critical',
+                'value' => 'Critical',
+            ],
+            [
+                'label' => 'Normal',
+                'value' => 'Normal',
+            ],
+            [
+                'label' => 'Low priority',
+                'value' => 'Low priority',
+            ]
+        ];
+        return view('create', compact('statuses'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required'
+        ]);
+
+        $task = new Task();
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->status = $request->status;
+        $task->save();
+        return redirect()->route('index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $task = Task::findOrFail($id);
+        $statuses = [
+            [
+                'label' => 'Critical',
+                'value' => 'Critical',
+            ],
+            [
+                'label' => 'Normal',
+                'value' => 'Normal',
+            ],
+            [
+                'label' => 'Low priority',
+                'value' => 'Low priority',
+            ]
+        ];
+        return view('edit', compact('statuses', 'task'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $task = Task::findOrFail($id);
+        $request->validate([
+            'title' => 'required'
+        ]);
+
+        $task->title = $request->title;
+        $task->description = $request->description;
+        $task->status = $request->status;
+        $task->save();
+        return redirect()->route('index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $task = Task::findOrFail($id);
+        $task->delete();
+        return redirect()->route('index');
+    }
+}
---
+++ sara1/resources/views/index.blade.php
@@ -0,0 +1,89 @@
+@extends('layout')
+
+@section('main-content')
+    <div>
+        <div class="float-start">
+            <h4 class="pb-3">My Tasks</h4>
+        </div>
+        <div class="float-end">
+            <a href="{{ route('task.create') }}" class="btn btn-info">
+               <i class="fa fa-plus-circle"></i> Create Task
+            </a>
+        </div>
+        <div class="clearfix"></div>
+    </div>
+
+    @foreach ($tasks as $task)
+        <div class="card mt-3">
+            <h5 class="card-header">
+                @if ($task->status === 'Normal')
+                    {{ $task->title }}
+                @else
+                    {{ $task->title }}
+                @endif
+
+                <span class="badge rounded-pill bg-warning text-dark">
+                    {{ $task->created_at->diffForHumans() }}
+                </span>
+            </h5>
+
+            <div class="card-body">
+                <div class="card-text">
+                    <div class="float-start">
+                        @if ($task->status === 'Critical')
+                            {{ $task->description }}
+                        @else
+                           {{ $task->description }}
+                        @endif
+                        <br>
+                        @if ($task->status === 'Critical')
+                        <span class="badge rounded-pill bg-info text-dark">
+                            Critical
+                        </span>
+                                            @endif
+                                            @if ($task->status === 'Normal')
+                                            <span class="badge rounded-pill bg-info text-dark">
+                                                Normal
+                                            </span>            @endif
+                                            @if ($task->status === 'Low priority')
+                                            <span class="badge rounded-pill bg-info text-dark">
+                                                Low priority
+                                            </span>            @endif
+                    
+
+
+                        <small>Last Updated - {{ $task->updated_at->diffForHumans() }} </small>
+                    </div>
+                    <div class="float-end">
+                        <a href="{{ route('task.edit', $task->id) }}" class="btn btn-success">
+                           <i class="fa fa-edit"></i>
+                        </a>
+
+                        <form action="{{ route('task.destroy', $task->id) }}" style="display: inline" method="POST" onsubmit="return confirm('Are you sure to delete ?')">
+                            @csrf
+                            @method('DELETE')
+
+                            <button type="submit" class="btn btn-danger">
+                                <i class="fa fa-trash"></i>
+                            </button>
+                        </form>
+
+                    </div>
+                    <div class="clearfix"></div>
+                </div>
+            </div>
+        </div>
+    @endforeach
+
+    @if (count($tasks) === 0)
+        <div class="alert alert-danger p-2">
+            No Task Found. Please Create one task
+            <br>
+            <br>
+            <a href="{{ route('task.create') }}" class="btn btn-info">
+                <i class="fa fa-plus-circle"></i> Create Task
+             </a>
+        </div>
+    @endif
+
+@endsection
